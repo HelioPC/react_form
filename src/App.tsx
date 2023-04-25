e1af419020caba6604dbbdd206953600354beb32
Eliúde Vemba <sweeteliude@gmail.com>
2023-04-25 23:20:06 +0100

    Fix routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import { FinalPage } from "./pages/FinalPage";
 
 import { FormStep1 } from "./pages/FormStep1";
@@ -7,14 +7,14 @@ import { FormStep3 } from "./pages/FormStep3";
 
 const AppRouter = () => {
     return(
-        <BrowserRouter>
+        <HashRouter>
             <Routes>
                 <Route path='/' element={<FormStep1 />} />
                 <Route path='/step2' element={<FormStep2 />} />
                 <Route path='/step3' element={<FormStep3 />} />
                 <Route path='/infos' element={<FinalPage />} />
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
     );
 }
 
